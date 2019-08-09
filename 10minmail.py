557aeab9503c9f3e04ac593f558fe049f6393500
maltejur <48161361+maltejur@users.noreply.github.com>
2019-08-09 21:54:09 +0200

    now windows-compatible
    still have to test if the program now works with mac

--- 10minmail.py
+++ 10minmail.py
@@ -1,28 +1,27 @@
 #!/usr/local/bin/python3
 # -*- coding: utf-8 -*-
 
-#https://github.com/liris/websocket-client
-## sudo pip install --user websocket-client
-from websocket import create_connection
+# depends on websocket-client and pyperclip
+import websocket
+import pyperclip
+import sys
+import os
+from json import loads
+from datetime import datetime
 
 class mailbox(object):
 	"""10 minute mailbox"""
 	def __init__(self):
 		super(mailbox, self).__init__()
-		self.ws = create_connection("wss://dropmail.me/websocket")
+		self.ws = websocket.WebSocket()
+		self.ws.connect("wss://dropmail.me/websocket")
 		self.next = self.ws.recv
 		self.close = self.ws.close
 		self.email = self.next()[1:].split(":")[0]
 		self.next()
 
 def main(box):
-	#stdlib
-	import sys
-	from json import loads
-	from subprocess import call
-	from datetime import datetime
-	
-	call(["echo '{0}' | pbcopy".format(box.email)], shell=True)
+	pyperclip.copy(box.email)
 	print (box.email+" was copied to clipboard")
 	while True:
 		result =  box.next()
@@ -34,8 +33,6 @@ def main(box):
 			print("Recieved:{1} {0}\n".format(result, datetime.now()))
 
 if __name__ == '__main__':
-	import os
-	print("PID: {0}\nIf you can't quite, run 'kill {0}'\n".format(os.getpid()))
 	try:
 		box = mailbox()
 		main(box)
--- README.md
+++ README.md
@@ -5,11 +5,18 @@
 
 This is a command-line python client which generates an email address that lasts 10 minutes. It is not traceable back to you. It is a wrapper on the WebSocket endpoint exposed by this [disposable e-mail service](https://dropmail.me/en/).
 
-# *PYTHON 3*
-## _Mac OSx_
-
+## Installing Dependencies
+### Mac OSx
+`sudo pip install --user websocket-client`
+<br>
+`sudo pip install --user pyperclip`
+### Windows
+`pip install websocket-client`
+<br>
+`pip install pyperclip`
+## Usage
 `python3 10minmail.py`
-
+<br><br>
 It will then copy the resulting email to your clipboard
 <br>
 Any messages recieved will be printed to stdout.
